1a5eb252c31af81cabcf5a9857ecc9b71fc99507
vr.bharadwaj <vr.bharadwaj@358c9309-8846-0410-a16c-f54bbacb4d78>
2008-04-02 11:16:08 +0000

    Modified fib test application so that _start appears first in the object code. This is just because the elf loaded will not start from main yet.

--- ref/03-fib/main.c
+++ ref/03-fib/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int fib(int x);
+int _start(){
+  return fib(12);
+}
 int fib(int x){
   if(x == 0)
     return 0;
@@ -7,8 +10,5 @@ int fib(int x){
     return 1;
   else if(x > 1)
     return fib(x-1) + fib(x-2);
-}
-int _start(){
-  return fib(20);
 }
 
